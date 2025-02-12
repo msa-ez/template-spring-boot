5e371b2600821e087ab964479b16197fb22e93bd
kibumpark <top5w5@naver.com>
2025-02-12 14:08:31 +0900

    123

--- .template/test/expected/상품/src/test/java/stmall/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package stmall.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import stmall.상품Application;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { 상품Application.class })
-public class CucumberSpingConfiguration {}
--- .template/test/expected/주문/src/test/java/stmall/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package stmall.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import stmall.주문Application;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { 주문Application.class })
-public class CucumberSpingConfiguration {}
