8639c3ea6d024f954fa3965dd0aff2588f0f851c
kyusoo <rbtn110@uengine.org>
2025-04-06 13:07:01 +0900

    frontend 변경

---
+++ for-model/frontend/src/components/View.vue
@@ -0,0 +1,68 @@
+forEach: View
+fileName: {{namePascalCase}}View.vue
+except: {{#checkViewType dataProjection}}{{/checkViewType}}
+---
+<template>
+    <v-data-table
+        :headers="headers"
+        :items="items"
+        :items-per-page="5"
+        class="elevation-1"
+    ></v-data-table>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { VDataTable } from 'vuetify/labs/VDataTable'
+
+export default {
+    name: '{{namePascalCase}}View',
+    components: {
+        VDataTable,
+    },
+    props: {
+        value: Object,
+        editMode: Boolean,
+        isNew: Boolean
+    },
+    setup() {
+        const headers = ref([
+            // 필드 디스크립터를 기반으로 헤더 설정
+            {{#fieldDescriptors}}
+            { title: "{{nameCamelCase}}", key: "{{nameCamelCase}}" },
+            {{/fieldDescriptors}}
+        ]);
+
+        const items = ref([]);
+
+        onMounted(async () => {
+            try {
+                const response = await axios.get('/{{namePlural}}');
+                const data = response.data._embedded.{{namePlural}};
+                data.forEach(obj => {
+                    obj.id = obj._links.self.href.split("/").pop();
+                });
+                items.value = data;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        });
+
+        return {
+            headers,
+            items
+        };
+    }
+}
+</script>
+
+<function>
+    window.$HandleBars.registerHelper('checkViewType', function (dataProjection) {
+        if(dataProjection == 'cqrs'){
+            return false;
+        } else {
+            return true;
+        }
+    })
+</function>
---
+++ for-model/frontend/src/layouts/components/DrawerContent.vue
@@ -0,0 +1,43 @@
+forEach: Model
+fileName: DrawerContent.vue
+---
+<script setup>
+import {
+  VerticalNavLink,
+  VerticalNavSectionTitle,
+} from '@layouts'
+</script>
+
+<template>
+    <ul>
+{{#boundedContexts}}
+        <VerticalNavSectionTitle :item="{ heading: '{{#ifNotNull displayName namePascalCase}}{{/ifNotNull}}' }" />
+    {{#aggregates}}
+        <VerticalNavLink
+            :item="{
+                title: '{{#ifNotNull displayName namePascalCase}}{{/ifNotNull}}',
+                to: '/{{namePlural}}',
+            }"
+        />
+    {{/aggregates}}
+    {{#views}}
+        <VerticalNavLink
+            :item="{
+                title: '{{#ifNotNull displayName namePascalCase}}{{/ifNotNull}}',
+                to: '/{{namePlural}}',
+            }"
+        />
+    {{/views}}
+{{/boundedContexts}}
+    </ul>
+</template>
+
+<function>
+window.$HandleBars.registerHelper('ifNotNull', function (displayName, name) {
+    if(displayName){
+        return displayName;
+    }else{
+        return name;
+    }
+})
+</function>
