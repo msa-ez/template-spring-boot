7fd59d9a2379c121a5a0279bb9f2f02cc4edb194
kyusoo <rbtn110@uengine.org>
2025-04-22 16:10:46 +0900

    어노테이션 위치 수정

--- src/main/java/com/eventstorming/domain/AggregateRoot.java
+++ src/main/java/com/eventstorming/domain/AggregateRoot.java
@@ -30,12 +30,12 @@ public class {{namePascalCase}} {{#checkExtends aggregateRoot.entities.relations
 
 
     {{#aggregateRoot.fieldDescriptors}}
-    {{^isVO}}
-    {{#isKey}}
-    @Id
-    {{#checkClassType ../aggregateRoot.fieldDescriptors}}{{/checkClassType}}
-    {{/isKey}}
-    {{/isVO}}
+{{^isVO}}
+{{#isKey}}
+@Id
+{{#checkClassType ../aggregateRoot.fieldDescriptors}}{{/checkClassType}}
+{{/isKey}}
+{{/isVO}}
     {{#isLob}}@Lob{{/isLob}}
     {{#if (isPrimitive className)}}{{#isList}}{{/isList}}{{/if}}
     {{#checkFieldType className isVO namePascalCase isKey ../aggregateRoot.entities.relations}}{{/checkFieldType}}
